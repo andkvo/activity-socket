0418a9507df00296c4af72f8e6d5e8aeb9efed6b
Andrew Kvochick <andrew@pushpress.com>
2020-05-19 14:29:41 -0400

    working websocket demo with react stream

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { w3cwebsocket as W3CWebSocket, IMessageEvent } from "websocket";
+import { Image, List, Transition } from 'semantic-ui-react';
+import _ from 'lodash';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const client = new W3CWebSocket('ws://127.0.0.1:8000');
+
+class State {
+  messages: Array<IMessage> = [];
+}
+
+interface IMessage {
+  timeStamp: number;
+  data: string;
+}
+
+class App extends Component<{}, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = { messages: [] };
+  }
+
+  componentWillMount() {
+    client.onopen = () => {
+      console.log('WebSocket Client Connected');
+    };
+    client.onmessage = (message: IMessageEvent) => {
+      console.log(message);
+      const recdDate = new Date().valueOf();
+      this.setState((prevState) => ({
+        messages: prevState.messages.concat([{ data: message.data.toString(), timeStamp: recdDate }]).sort((a: any, b: any) => b.timeStamp - a.timeStamp)
+      }))
+    };
+  }
+  
+  render() {
+    return (
+      <div>
+        <Transition.Group
+          as={List}
+          duration={200}
+          divided
+          size='huge'
+          verticalAlign='middle'
+          animation='slide down'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {this.state.messages.map((m) => (
+            <List.Item key={(m as any).timeStamp}>
+              <List.Content header={_.startCase(`${(m as any).timeStamp} - ${m.data.toString()}`)} />
+            </List.Item>
+          ))}
+        </Transition.Group>
+      </div>
+    );
+  }
 }
 
 export default App;
